d7cbe9b4508a0d780e0d2f20efa1e2ecfe03f316
mrfrosk <heroker44@gmail.com>
2022-07-16 21:12:24 +0300

    Убрал костыли с try except, изменил получения текста из комманды.

---
+++ Get_filtered_messages.py
@@ -0,0 +1,14 @@
+from asyncio import set_event_loop, new_event_loop
+
+from data import api_id, api_hash, app_name
+from telethon.sync import TelegramClient
+
+
+def get_filtered_messages(search_message):
+    messages = []
+    set_event_loop(new_event_loop())
+    with TelegramClient(app_name, api_id, api_hash) as client:
+        for message in client.iter_messages("Пруфы Жожеков"):
+            if isinstance(message.text, str) and search_message in message.text:
+                messages.append(message.text)
+    return messages
--- main.py
+++ main.py
@@ -1,24 +1,24 @@
-import asyncio
-
 from data import token
 import telebot
-from Get_Messages import get_messages
+from Get_filtered_messages import get_filtered_messages
 
 bot = telebot.TeleBot(token)
 
 
-
 @bot.message_handler(commands=["get"])
 def handle_text(message):
-    for i in get_messages(message):
-        try:
-            bot.send_message(message.chat.id, i)
-        except telebot.apihelper.ApiTelegramException:
-            continue
+    messages = [message for message in get_filtered_messages(message.text[5:]) if message != ""]
+    if messages:
+        for message in messages:
+            bot.send_message(message.chat.id, message)
+    else:
+        bot.send_message(message.chat.id, "сообщений содержаших такую строку не было обнаружено")
 
 
 @bot.message_handler(commands=["help"])
 def bot_help(message):
-    bot.send_message(message.chat.id, "что,s отфильтровать посты комманда /get: пример\n"
+    bot.send_message(message.chat.id, "чтобы  отфильтровать сообщения комманда /get пример:\n"
                                       "/get О кулаках")
+
+
 bot.polling()
