7b816dd4de5feb1910299af094859f1af78b5ea9
mrfrosk <heroker44@gmail.com>
2022-07-16 21:26:40 +0300

    Перенос названия канала в отделную переменную в data.py, убрал конвертацию str(message.text)

--- Get_filtered_messages.py
+++ Get_filtered_messages.py
@@ -1,6 +1,6 @@
 from asyncio import set_event_loop, new_event_loop
 
-from data import api_id, api_hash, app_name
+from data import api_id, api_hash, app_name, channel_name
 from telethon.sync import TelegramClient
 
 
@@ -8,7 +8,7 @@ def get_filtered_messages(search_message):
     messages = []
     set_event_loop(new_event_loop())
     with TelegramClient(app_name, api_id, api_hash) as client:
-        for message in client.iter_messages("Пруфы Жожеков"):
+        for message in client.iter_messages(channel_name):
             if isinstance(message.text, str) and search_message in message.text:
                 messages.append(message.text)
     return messages
